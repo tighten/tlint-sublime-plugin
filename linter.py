57f6c47ffc39c0d660509a1f919431a5319b4abf
Andrew Morgan <andrewmile@gmail.com>
2019-06-28 09:33:03 -0500

    Update linter to read from file on disk

--- linter.py
+++ linter.py
@@ -1,7 +1,8 @@
 from SublimeLinter.lint import ComposerLinter
 
 class Tlint(ComposerLinter):
-    cmd = ('tlint', 'lint', '${file}', '--json')
+    cmd = ('tlint', 'lint', '${file_on_disk}', '--json')
+    tempfile_suffix = '-'
     regex = r'{\"line\":(?P<line>[0-9]+),\"message\":\"(?P<message>.*?)\",\"source\":\"(?P<source>.*?)\"}'
     multiline = True
     defaults = {
